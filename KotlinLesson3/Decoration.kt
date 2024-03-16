b391a6ae266297a6ef36624268e1ad167d8e1e49
graciabetty <graciadanielj@gmail.com>
2024-03-16 17:43:45 -0400

    files added

---
+++ KotlinLesson3/Decoration.kt
@@ -0,0 +1,37 @@
+package example.myapp.decor
+
+
+
+data class Decoration(val rocks: String) {
+}
+// Here is a data class with 3 properties.
+data class Decoration2(val rocks: String, val wood: String, val diver: String){
+}
+
+fun makeDecorations() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+// Assign all properties to variables.
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+}
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name:String) = println("My favorite $name")
+    }
+}
+
+fun main() {
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
+}
