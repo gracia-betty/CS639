246aacd6a72c7d189366ceb51f50aeabe1c31119
graciabetty <graciadanielj@gmail.com>
2024-03-23 20:34:30 -0400

    files added

---
+++ Kotlinlesson4BirthdayCardApp/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlinlesson4 Birthday Card App"
+include(":app")
